e798daca8078f83dd105dd1683305278005b1750
Peter Baldwin <pbaldwin@clearfoundation.com>
2012-11-13 01:15:37 +0000

    - Prepped for beta build

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'openldap_directory';
-$app['version'] = '1.2.3';
+$app['version'] = '1.4.3';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
