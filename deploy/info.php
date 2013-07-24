8587b8f9f60f99ff51f00210ad5350813bce38b6
Peter Baldwin <pbaldwin@clearfoundation.com>
2013-07-24 15:13:47 +0000

    - Prepped for errata update

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'openldap_directory';
-$app['version'] = '1.4.22';
+$app['version'] = '1.4.35';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
