05bba8cd1a349afc9a5fb2fe98598f9fbb2096ca
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:34:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/is/openldap_directory_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['openldap_directory_accounts_access'] = 'reikninga aðgang';
+$lang['openldap_directory_accounts_access_invalid'] = 'Reikningar aðgangur er ógilt.';
+$lang['openldap_directory_accounts_dn'] = 'reikninga DN';
+$lang['openldap_directory_anonymous'] = 'Anonymous';
+$lang['openldap_directory_app_description'] = 'The Directory Server veitir upplýsingar um notendur, hópa og kerfum. Þú getur veitt lesa aðeins aðgang að skránni til að fella þriðja umsókn aðila, bæði á staðnum og í skýinu.';
+$lang['openldap_directory_app_name'] = 'Directory Server';
+$lang['openldap_directory_initialize_help'] = 'Þegar frumstilla Directory Server skaltu velja stöð lén sem þú notar fyrir fyrirtæki þínu. Þetta lén er notað til auðkenningar og aðra kerfi þjónustu.';
+$lang['openldap_directory_openldap_directory_driver'] = 'OpenLDAP Directory Driver';
+$lang['openldap_directory_openldap_directory_driver_description'] = 'The OpenLDAP Ökumaðurinn fyrir þjónustu skrá';
+$lang['openldap_directory_password_access'] = 'Lykilorð Access';
+$lang['openldap_directory_policies'] = 'Reglur';
