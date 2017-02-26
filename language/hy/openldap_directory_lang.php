a778bc4c62a7e8c584abdfef1f2856a27ae0578a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:30:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hy/openldap_directory_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['openldap_directory_accounts_access'] = 'Հաշիվներ Access';
+$lang['openldap_directory_accounts_access_invalid'] = 'Հաշիվներ հասանելիության անվավեր է:';
+$lang['openldap_directory_accounts_dn'] = 'Հաշիվներ DN';
+$lang['openldap_directory_anonymous'] = 'Անանուն';
+$lang['openldap_directory_app_description'] = 'Տեղեկագրքի Server տրամադրում է տեղեկատվություն օգտագործողներին, խմբերի եւ համակարգերի. Դուք կարող եք ապահովել կարդալ միայն մուտք գործել Ձեր դիրեկտորիայի ինտեգրվելու երրորդ կողմի ծրագրեր, այնպես էլ տեղական եւ ամպի.';
+$lang['openldap_directory_app_name'] = 'Ուղեցույց Server';
+$lang['openldap_directory_initialize_help'] = 'Երբ Նախապատրաստվում ձեր Ուղեցույց Server համար, խնդրում ենք ընտրել, բազային տիրույթում է, որ դուք օգտագործել Ձեր կազմակերպության. Այս տիրույթը օգտագործվում է վավերացման եւ այլ համակարգի ծառայություններ.';
+$lang['openldap_directory_openldap_directory_driver'] = 'OpenLDAP Ուղեցույց Վարորդ';
+$lang['openldap_directory_openldap_directory_driver_description'] = 'The OpenLDAP վարորդ տեղեկատու ծառայությունների';
+$lang['openldap_directory_password_access'] = 'Գաղտնաբառ Access';
+$lang['openldap_directory_policies'] = 'քաղաքականությունը';
