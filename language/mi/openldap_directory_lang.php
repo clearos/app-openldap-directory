d4a4b7ae94dd1e93b1c3e9ccd52d640668194ce1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:36:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/openldap_directory_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['openldap_directory_accounts_access'] = 'pūkete Access';
+$lang['openldap_directory_accounts_access_invalid'] = 'He muhu pūkete uru.';
+$lang['openldap_directory_accounts_dn'] = 'pūkete DN';
+$lang['openldap_directory_anonymous'] = 'Anonymous';
+$lang['openldap_directory_app_description'] = 'whakarato te Tūmau Whaiaronga mōhiohio i runga i ngā kaiwhakamahi, rōpū me ngā pūnaha. Ka taea e koe te whakarato te pānui i te uru anake ki tou whaiaronga ki te whakauru i tono rōpū tuatoru e rua rohe me i te kapua.';
+$lang['openldap_directory_app_name'] = 'Tūmau Whaiaronga';
+$lang['openldap_directory_initialize_help'] = 'A, no te arawhiti koutou Tūmau Whaiaronga, tēnā tīpako i te rohe turanga e whakamahi koe mo tou whakahaere. Kei te whakamahia tēnei rohe mō te motuhēhēnga me ētahi atu ratonga pūnaha.';
+$lang['openldap_directory_openldap_directory_driver'] = 'Atekōkiri OpenLDAP Whaiaronga';
+$lang['openldap_directory_openldap_directory_driver_description'] = 'Te taraiwa OpenLDAP mō ngā ratonga whaiaronga';
+$lang['openldap_directory_password_access'] = 'Access kupuhipa';
+$lang['openldap_directory_policies'] = 'kaupapa here';
