82bbe057c44bcf8cbe4f43ed01abb06063d24cc9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:32:12 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/openldap_directory_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['openldap_directory_accounts_access'] = 'nkhani Access';
+$lang['openldap_directory_accounts_access_invalid'] = 'Nkhani mwayi ndi opanda pake.';
+$lang['openldap_directory_accounts_dn'] = 'nkhani DN';
+$lang['openldap_directory_anonymous'] = 'mosaonetsera';
+$lang['openldap_directory_app_description'] = 'The Directory Server amapereka mauthenga pa Intaneti, magulu ndi kachitidwe. Mungapereke kuwerenga kupeza okha Directory anu cigawo wachitatu ntchito chipani kwawoko ndi mtambo.';
+$lang['openldap_directory_app_name'] = 'Directory Server';
+$lang['openldap_directory_initialize_help'] = 'Pamene potsegula Directory Server wanu, chonde sankhani m&#39;munsi ankalamulira kuti ntchito bungwe lanu. ankalamulira ichi ntchito kutsimikizika ndi zina dongosolo.';
+$lang['openldap_directory_openldap_directory_driver'] = 'OpenLDAP Directory Yoyendetsa';
+$lang['openldap_directory_openldap_directory_driver_description'] = 'Woyendetsa OpenLDAP misonkhano Directory';
+$lang['openldap_directory_password_access'] = 'achinsinsi Access';
+$lang['openldap_directory_policies'] = 'ndondomeko';
