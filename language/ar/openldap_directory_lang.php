ac1e694f930c9bd84c1334a0afff20db01f7ed9c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:30:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ar/openldap_directory_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['openldap_directory_accounts_access'] = 'حسابات الوصول';
+$lang['openldap_directory_accounts_access_invalid'] = 'حسابات الوصول غير صالح.';
+$lang['openldap_directory_accounts_dn'] = 'حسابات DN';
+$lang['openldap_directory_anonymous'] = 'مجهول';
+$lang['openldap_directory_app_description'] = 'يوفر خادم دليل المعلومات عن المستخدمين والمجموعات والأنظمة. يمكنك توفير الوصول للقراءة فقط إلى دليل لدمج تطبيقات الطرف الثالث محليا وفي السحابة.';
+$lang['openldap_directory_app_name'] = 'خادم الدليل';
+$lang['openldap_directory_initialize_help'] = 'عند تهيئة خادم الدليل الخاص بك، يرجى تحديد المجال قاعدة التي تستخدمها لمؤسستك. ويستخدم هذا مجال المصادقة وخدمات الأنظمة الأخرى.';
+$lang['openldap_directory_openldap_directory_driver'] = 'سائق ب OpenLDAP دليل';
+$lang['openldap_directory_openldap_directory_driver_description'] = 'السائق ب OpenLDAP لخدمات الدليل';
+$lang['openldap_directory_password_access'] = 'وصول كلمة السر';
+$lang['openldap_directory_policies'] = 'سياسات';
