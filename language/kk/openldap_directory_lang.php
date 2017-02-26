dee5f4be1c859cf00e61e4216c4f6dab065cf5eb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:35:31 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kk/openldap_directory_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['openldap_directory_accounts_access'] = 'Access Accounts';
+$lang['openldap_directory_accounts_access_invalid'] = 'қол жарамсыз шоттары.';
+$lang['openldap_directory_accounts_dn'] = 'DN тіркелгілер';
+$lang['openldap_directory_anonymous'] = 'Аноним';
+$lang['openldap_directory_app_description'] = 'Directory Server пайдаланушылар, топтар мен жүйелер туралы ақпарат береді. Сіз жергілікті және бұлтта үшінші тарап қолданбаларын біріктіруге Сіздің каталогына оқу қатынауы ғана қамтамасыз ете алады.';
+$lang['openldap_directory_app_name'] = 'Directory Server';
+$lang['openldap_directory_initialize_help'] = 'Егер каталог сервері инициализациялау кезде, ұйымыңыздың пайдалануға базалық доменді таңдаңыз. Бұл домен және аутентификациялау басқа жүйе қызметтері үшін пайдаланылады.';
+$lang['openldap_directory_openldap_directory_driver'] = 'OpenLDAP Directory Driver';
+$lang['openldap_directory_openldap_directory_driver_description'] = 'каталогтар қызметтері үшін OpenLDAP драйвер';
+$lang['openldap_directory_password_access'] = 'Құпия сөз Access';
+$lang['openldap_directory_policies'] = 'Саясат';
