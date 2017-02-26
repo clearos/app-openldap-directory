be7e4f9c797efff8e02c62b6c003cef416f8703b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:37:11 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mn/openldap_directory_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['openldap_directory_accounts_access'] = 'Access данс';
+$lang['openldap_directory_accounts_access_invalid'] = 'Бүртгэлд нэвтрэх хүчин төгөлдөр бус байна.';
+$lang['openldap_directory_accounts_dn'] = 'DN данс';
+$lang['openldap_directory_anonymous'] = 'Anonymous';
+$lang['openldap_directory_app_description'] = 'Лавлах сервер хэрэглэгч, бүлэг болон системийн талаар мэдээлэл өгнө. Та орон нутгийн болон үүлэн дотор аль аль нь гуравдагч програмуудыг нэгтгэх таны санд зөвхөн унших хандалтыг өгч болох юм.';
+$lang['openldap_directory_app_name'] = 'лавлах Server';
+$lang['openldap_directory_initialize_help'] = 'Таны лавлах серверийг эхлүүлж байхад, та өөрийн байгууллагын ашигладаг үндсэн домэйн нэр сонгоно уу. Энэ нь домэйн танилтад болон бусад системийн үйлчилгээнд ашиглаж байна.';
+$lang['openldap_directory_openldap_directory_driver'] = 'OpenLDAP лавлах жолооч';
+$lang['openldap_directory_openldap_directory_driver_description'] = 'лавлах үйлчилгээ OpenLDAP драйвер';
+$lang['openldap_directory_password_access'] = 'Нууц үг нэвтрэх';
+$lang['openldap_directory_policies'] = 'Бодлого';
