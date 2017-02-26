37e159873faa437eb3eedcafd13262e159353605
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:38:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ro/openldap_directory_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['openldap_directory_accounts_access'] = 'conturi de acces';
+$lang['openldap_directory_accounts_access_invalid'] = 'accesul conturilor este nevalid.';
+$lang['openldap_directory_accounts_dn'] = 'conturi DN';
+$lang['openldap_directory_anonymous'] = 'Anonim';
+$lang['openldap_directory_app_description'] = 'Directory Server oferă informații despre utilizatori, grupuri și sisteme. Vă poate oferi acces de citire numai la directorul dvs. pentru a integra aplicații terțe părți, atât la nivel local și în nor.';
+$lang['openldap_directory_app_name'] = 'directory Server';
+$lang['openldap_directory_initialize_help'] = 'Atunci când inițializează Directory Server, vă rugăm să selectați domeniul de bază pe care îl utilizați pentru organizația dumneavoastră. Acest domeniu este utilizat pentru autentificare și alte servicii de sistem.';
+$lang['openldap_directory_openldap_directory_driver'] = 'OpenLDAP Directory driver';
+$lang['openldap_directory_openldap_directory_driver_description'] = 'Conducătorul auto OpenLDAP pentru serviciile de director';
+$lang['openldap_directory_password_access'] = 'parola de acces';
+$lang['openldap_directory_policies'] = 'politicile';
