dfe01e195870f9cfeefcb62aaadcf875395c8411
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:36:30 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lv/openldap_directory_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['openldap_directory_accounts_access'] = 'konti Access';
+$lang['openldap_directory_accounts_access_invalid'] = 'Konti piekļuve ir nederīgs.';
+$lang['openldap_directory_accounts_dn'] = 'konti DN';
+$lang['openldap_directory_anonymous'] = 'Anonīms';
+$lang['openldap_directory_app_description'] = 'Direktorija Server nodrošina informāciju par lietotāju, grupu un sistēmas. Jūs varat sniegt tikai lasīšanas piekļuvi jūsu direktorijā integrēt trešo personu pieteikumus gan vietējā, gan mākonī.';
+$lang['openldap_directory_app_name'] = 'Directory Server';
+$lang['openldap_directory_initialize_help'] = 'Kad inicializēšana jūsu Directory Server, lūdzu, izvēlieties bāzes domēnu, ko izmantojat, lai jūsu organizācijai. Šis domēns tiek izmantots autentifikācijas un citām sistēmas pakalpojumiem.';
+$lang['openldap_directory_openldap_directory_driver'] = 'OpenLDAP Directory Driver';
+$lang['openldap_directory_openldap_directory_driver_description'] = 'OpenLDAP draiveris uzziņu pakalpojumus';
+$lang['openldap_directory_password_access'] = 'paroles Access';
+$lang['openldap_directory_policies'] = 'Politikas';
