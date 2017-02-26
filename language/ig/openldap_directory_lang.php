4d028dedaf76f9c43ebf22751ec16a41c38e5a93
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:34:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ig/openldap_directory_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['openldap_directory_accounts_access'] = 'akụkọ Access';
+$lang['openldap_directory_accounts_access_invalid'] = 'Ihe ndekọ ohere bụ ghara ịdị irè.';
+$lang['openldap_directory_accounts_dn'] = 'akụkọ DN';
+$lang['openldap_directory_anonymous'] = 'amaonye';
+$lang['openldap_directory_app_description'] = 'The Directory Server enye ihe ọmụma na ọrụ, dị iche iche na usoro. Ị nwere ike inye na-agụ naanị ohere gị directory iwekota atọ ọzọ ngwa ma n&#39;ógbè ha ma na ígwé ojii.';
+$lang['openldap_directory_app_name'] = 'Directory Server';
+$lang['openldap_directory_initialize_help'] = 'Mgbe initializing gị Directory Server, biko họrọ isi ngalaba ahụ na ị na-eji maka gị nzukọ. Nke a domain-eji maka Nyocha na ndị ọzọ na usoro ọrụ.';
+$lang['openldap_directory_openldap_directory_driver'] = 'OpenLDAP Directory Driver';
+$lang['openldap_directory_openldap_directory_driver_description'] = 'The OpenLDAP ọkwọ ụgbọala maka directory ọrụ';
+$lang['openldap_directory_password_access'] = 'Password Access';
+$lang['openldap_directory_policies'] = 'atumatu';
