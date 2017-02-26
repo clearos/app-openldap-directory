a86d9f431ec477d0a67f4c2091177c75a1907192
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:36:21 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/openldap_directory_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['openldap_directory_accounts_access'] = 'Sąskaitos Prisijunkite';
+$lang['openldap_directory_accounts_access_invalid'] = 'Sąskaitos prieiga yra neteisinga.';
+$lang['openldap_directory_accounts_dn'] = 'Sąskaitos DN';
+$lang['openldap_directory_anonymous'] = 'Anoniminis';
+$lang['openldap_directory_app_description'] = 'Rodyklės Server suteikia informaciją apie vartotojus, grupes ir sistemas. Galite teikti skaityti tik prieigą prie jūsų kataloge integruoti trečiųjų šalių programas tiek lokaliai, tiek į debesis.';
+$lang['openldap_directory_app_name'] = 'Directory Server';
+$lang['openldap_directory_initialize_help'] = 'Kai Inicijuojama savo Directory Server, pasirinkite bazinę domeną, kurį naudojate savo organizacijoje. Šis domenas yra naudojama autentiškumo ir kitų sistemos paslaugomis.';
+$lang['openldap_directory_openldap_directory_driver'] = 'OpenLDAP katalogo Vairuotojas';
+$lang['openldap_directory_openldap_directory_driver_description'] = 'OpenLDAP vairuotojas abonentus teikimo paslaugos';
+$lang['openldap_directory_password_access'] = 'slaptažodis Prieiga';
+$lang['openldap_directory_policies'] = 'politika';
