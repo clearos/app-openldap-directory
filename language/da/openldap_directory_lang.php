fd6245b654d4cdc02a3d2b53e77e496a84065f8a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:32:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/da/openldap_directory_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['openldap_directory_accounts_access'] = 'regnskab Adgang';
+$lang['openldap_directory_accounts_access_invalid'] = 'Konti adgang er ugyldig.';
+$lang['openldap_directory_accounts_dn'] = 'regnskab DN';
+$lang['openldap_directory_anonymous'] = 'Anonym';
+$lang['openldap_directory_app_description'] = 'Den Directory Server indeholder oplysninger om brugere, grupper og systemer. Du kan give skrivebeskyttet adgang til dit bibliotek for at integrere tredjeparts applikationer både lokalt og i skyen.';
+$lang['openldap_directory_app_name'] = 'directory Server';
+$lang['openldap_directory_initialize_help'] = 'Når initialisering din Directory Server, skal du vælge basen domæne, som du bruger til din organisation. Dette domæne bruges til godkendelse og andre systemtjenester.';
+$lang['openldap_directory_openldap_directory_driver'] = 'OpenLDAP Directory-driver';
+$lang['openldap_directory_openldap_directory_driver_description'] = 'Den OpenLDAP driver til bibliotekstjenester';
+$lang['openldap_directory_password_access'] = 'Password Adgang';
+$lang['openldap_directory_policies'] = 'Politikker';
