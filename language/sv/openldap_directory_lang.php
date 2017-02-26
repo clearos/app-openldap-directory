3ca9eddf7ec52b6c085f99800fba2d4ce1ae08b5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:40:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sv/openldap_directory_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['openldap_directory_accounts_access'] = 'konton Tillträde';
+$lang['openldap_directory_accounts_access_invalid'] = 'Konton tillgång är ogiltigt.';
+$lang['openldap_directory_accounts_dn'] = 'svarar DN';
+$lang['openldap_directory_anonymous'] = 'Anonym';
+$lang['openldap_directory_app_description'] = 'Directory Server innehåller information om användare, grupper och system. Du kan ge skrivskyddad tillgång till din katalog för att integrera tredje part både lokalt och i molnet.';
+$lang['openldap_directory_app_name'] = 'Directory Server';
+$lang['openldap_directory_initialize_help'] = 'När initierar din Directory Server, välj basen domän som du använder för din organisation. Denna domän används för autentisering och andra systemtjänster.';
+$lang['openldap_directory_openldap_directory_driver'] = 'OpenLDAP register Driver';
+$lang['openldap_directory_openldap_directory_driver_description'] = 'OpenLDAP drivrutinen för katalogtjänster';
+$lang['openldap_directory_password_access'] = 'Lösenord Tillträde';
+$lang['openldap_directory_policies'] = 'policies';
