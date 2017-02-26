8cc2babb26f627316ad2786f3fc2d486f31d82ec
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:32:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eo/openldap_directory_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['openldap_directory_accounts_access'] = 'kontoj Aliro';
+$lang['openldap_directory_accounts_access_invalid'] = 'Kontoj aliro estas nevalida.';
+$lang['openldap_directory_accounts_dn'] = 'kontoj DN';
+$lang['openldap_directory_anonymous'] = 'anonima';
+$lang['openldap_directory_app_description'] = 'La Dosierujo Server disponigas informojn pri uzantoj, grupoj kaj sistemoj. Vi povas havigi nurlegebla aliro al via dosierujo integri triaj aplikoj kaj loke kaj en la nubo.';
+$lang['openldap_directory_app_name'] = 'Katalogo Servilo';
+$lang['openldap_directory_initialize_help'] = 'Kiam inicializar vian Gvidlibro Servilo, bonvolu elekti la bazo havaĵo ke vi uzas por via organizo. Tiu domajno estas uzata por autenticación kaj alia sistemo servoj.';
+$lang['openldap_directory_openldap_directory_driver'] = 'OpenLdap Gvidlibro Ŝoforo';
+$lang['openldap_directory_openldap_directory_driver_description'] = 'La OpenLdap ŝoforo por dosierujo servoj';
+$lang['openldap_directory_password_access'] = 'Pasvorto Aliro';
+$lang['openldap_directory_policies'] = 'politikoj';
