a03f041e9ac364545f527ec9ddaca556a07bf01a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:30:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sq/openldap_directory_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['openldap_directory_accounts_access'] = 'llogaritë Qasja';
+$lang['openldap_directory_accounts_access_invalid'] = 'Llogaritë qasja është e pavlefshme.';
+$lang['openldap_directory_accounts_dn'] = 'llogaritë DN';
+$lang['openldap_directory_anonymous'] = 'anonim';
+$lang['openldap_directory_app_description'] = 'Drejtoria Server jep informacion mbi përdoruesit, grupet dhe sistemet. Ju mund të sigurojë të lexoni qasje vetëm në directory tuaj për të integruar aplikacionet e palës së tretë si në nivel lokal dhe në re.';
+$lang['openldap_directory_app_name'] = 'directory Server';
+$lang['openldap_directory_initialize_help'] = 'Kur fillimin Server tuaj Direktoria, ju lutem zgjidhni domain bazë që ju përdorni për organizatën tuaj. Ky domain është përdorur për autentikim dhe shërbime të tjera të sistemit.';
+$lang['openldap_directory_openldap_directory_driver'] = 'Shofer t&#39;OpenLdap Directory';
+$lang['openldap_directory_openldap_directory_driver_description'] = 'Shoferi t&#39;OpenLdap për shërbimet e direktoriumeve';
+$lang['openldap_directory_password_access'] = 'fjalëkalimi Access';
+$lang['openldap_directory_policies'] = 'politikat';
