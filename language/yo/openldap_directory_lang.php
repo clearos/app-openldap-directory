dc034e0f84d6ed6b1f45e3f5ca277eda2680be27
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:42:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yo/openldap_directory_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['openldap_directory_accounts_access'] = 'iroyin Access';
+$lang['openldap_directory_accounts_access_invalid'] = 'Àpamọ wiwọle jẹ invalid.';
+$lang['openldap_directory_accounts_dn'] = 'iroyin DN';
+$lang['openldap_directory_anonymous'] = 'Anonymous';
+$lang['openldap_directory_app_description'] = 'The Directory Server pese alaye lori awọn olumulo, awọn ẹgbẹ ati awọn ọna šiše. O le pese ka nikan wiwọle si rẹ liana lati ṣepọ eni keta ohun elo mejeeji tibile ati ninu awọsanma.';
+$lang['openldap_directory_app_name'] = 'Directory Server';
+$lang['openldap_directory_initialize_help'] = 'Nigba ti initializing rẹ Directory Server, jọwọ yan awọn mimọ domain ti o lo fun rẹ ètò. Yi ìkápá ti lo fun ìfàṣẹsí ati awọn miiran eto iṣẹ.';
+$lang['openldap_directory_openldap_directory_driver'] = 'OpenLDAP Directory Driver';
+$lang['openldap_directory_openldap_directory_driver_description'] = 'The OpenLDAP iwakọ fun liana iṣẹ';
+$lang['openldap_directory_password_access'] = 'ọrọigbaniwọle Access';
+$lang['openldap_directory_policies'] = 'imulo';
