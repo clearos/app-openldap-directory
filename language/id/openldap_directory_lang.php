c84c97f52a4ded34828ca10ef80ae48f7659f953
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:34:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/id/openldap_directory_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['openldap_directory_accounts_access'] = 'account Access';
+$lang['openldap_directory_accounts_access_invalid'] = 'Akses account tidak valid.';
+$lang['openldap_directory_accounts_dn'] = 'account DN';
+$lang['openldap_directory_anonymous'] = 'Anonim';
+$lang['openldap_directory_app_description'] = 'Directory Server memberikan informasi tentang pengguna, kelompok dan sistem. Anda dapat memberikan hanya membaca akses ke direktori Anda untuk mengintegrasikan aplikasi pihak ketiga baik lokal dan di awan.';
+$lang['openldap_directory_app_name'] = 'Directory Server';
+$lang['openldap_directory_initialize_help'] = 'Ketika menginisialisasi Directory Server Anda, silahkan pilih domain dasar yang Anda gunakan untuk organisasi Anda. domain ini digunakan untuk otentikasi dan layanan sistem lainnya.';
+$lang['openldap_directory_openldap_directory_driver'] = 'OpenLDAP Driver Directory';
+$lang['openldap_directory_openldap_directory_driver_description'] = 'Sopir OpenLDAP untuk layanan direktori';
+$lang['openldap_directory_password_access'] = 'Sandi Access';
+$lang['openldap_directory_policies'] = 'kebijakan';
