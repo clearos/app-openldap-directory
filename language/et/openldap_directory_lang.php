e7fa50a104fa2f83f8ef58e67866da7d7665d908
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:33:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/et/openldap_directory_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['openldap_directory_accounts_access'] = 'Kontod Access';
+$lang['openldap_directory_accounts_access_invalid'] = 'Kontod ühendus on vigane.';
+$lang['openldap_directory_accounts_dn'] = 'Kontod DN';
+$lang['openldap_directory_anonymous'] = 'anonüümne';
+$lang['openldap_directory_app_description'] = 'Kataloog Server annab teavet kasutajate, rühmade ja süsteeme. Võite pakkuda ainult lugemiseks juurdepääsu oma kataloogi integreerida kolmanda osapoole rakendusi nii kohalikul kui ka pilve.';
+$lang['openldap_directory_app_name'] = 'Kataloog Server';
+$lang['openldap_directory_initialize_help'] = 'Kui initializing oma kataloogi Server, palun valige baasi domeeni, et te kasutate oma organisatsioon. See domeen on kasutada autentimiseks ja teiste süsteemi teenuseid.';
+$lang['openldap_directory_openldap_directory_driver'] = 'OpenLDAP Kataloog Driver';
+$lang['openldap_directory_openldap_directory_driver_description'] = 'OpenLDAP juht kataloogiteenusteks';
+$lang['openldap_directory_password_access'] = 'Salasõna Access';
+$lang['openldap_directory_policies'] = 'poliitika';
