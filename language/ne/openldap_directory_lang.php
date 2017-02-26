2622e6fde399da35f4efbc2f9f4c7803d64bf183
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:37:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ne/openldap_directory_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['openldap_directory_accounts_access'] = 'पहुँच लेखा';
+$lang['openldap_directory_accounts_access_invalid'] = 'खाता पहुँच अमान्य छ।';
+$lang['openldap_directory_accounts_dn'] = 'लेखा DN';
+$lang['openldap_directory_anonymous'] = 'अज्ञात';
+$lang['openldap_directory_app_description'] = 'निर्देशिका सर्भर प्रयोगकर्ता, समूह र सिस्टम मा जानकारी प्रदान गर्दछ। तपाईं दुवै स्थानीय र क्लाउडमा तेस्रो पक्ष आवेदन एकीकृत आफ्नो निर्देशिका पढ्नका लागि मात्र पहुँच प्रदान गर्न सक्छन्।';
+$lang['openldap_directory_app_name'] = 'निर्देशिका सर्भर';
+$lang['openldap_directory_initialize_help'] = 'तपाईंको निर्देशिका सर्भर थालनी गर्दा, कृपया आधार डोमेन कि तपाईंले आफ्नो संगठनको लागि प्रयोग चयन गर्नुहोस्। यो डोमेन प्रमाणीकरण र अन्य सिस्टम सेवाहरूको लागि प्रयोग गरिन्छ।';
+$lang['openldap_directory_openldap_directory_driver'] = 'OpenLDAP निर्देशिका चालक';
+$lang['openldap_directory_openldap_directory_driver_description'] = 'निर्देशिका सेवाहरू लागि OpenLDAP चालक';
+$lang['openldap_directory_password_access'] = 'पासवर्ड पहुँच';
+$lang['openldap_directory_policies'] = 'नीतिहरू';
