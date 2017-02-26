380e2c6555a408fd4159a50f44213ff279cd40a8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:38:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ru/openldap_directory_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['openldap_directory_accounts_access'] = 'Учетные записи доступа';
+$lang['openldap_directory_accounts_access_invalid'] = 'Доступ к счетам является недействительным.';
+$lang['openldap_directory_accounts_dn'] = 'Счета DN';
+$lang['openldap_directory_anonymous'] = 'анонимный';
+$lang['openldap_directory_app_description'] = 'Сервер каталогов предоставляет информацию о пользователях, группах и систем. Вы можете предоставить доступ только для чтения к каталогу для интеграции приложений сторонних производителей как локально, так и в облаке.';
+$lang['openldap_directory_app_name'] = 'Directory Server';
+$lang['openldap_directory_initialize_help'] = 'При инициализации сервера каталога, пожалуйста, выберите домен базы, который вы используете для вашей организации. Этот домен используется для аутентификации и других системных служб.';
+$lang['openldap_directory_openldap_directory_driver'] = 'Драйвер OpenLDAP Каталог';
+$lang['openldap_directory_openldap_directory_driver_description'] = 'Драйвер OpenLDAP для служб каталогов';
+$lang['openldap_directory_password_access'] = 'Пароль доступа';
+$lang['openldap_directory_policies'] = 'полисы';
