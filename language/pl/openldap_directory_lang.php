d9609c0345a72f4bab26f20a66fe229d61a42841
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:38:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pl/openldap_directory_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['openldap_directory_accounts_access'] = 'Konta dostępu';
+$lang['openldap_directory_accounts_access_invalid'] = 'Dostęp do Konta jest nieprawidłowy.';
+$lang['openldap_directory_accounts_dn'] = 'Konta DN';
+$lang['openldap_directory_anonymous'] = 'Anonimowy';
+$lang['openldap_directory_app_description'] = 'Serwer katalog zawiera informacje na temat użytkowników, grup i systemów. Można podać tylko do odczytu dostępu do katalogu integrowania aplikacji firm trzecich, zarówno lokalnie, jak iw chmurze.';
+$lang['openldap_directory_app_name'] = 'directory Server';
+$lang['openldap_directory_initialize_help'] = 'Podczas inicjowania serwera katalogowego, wybierz domenę bazową, którego używasz w swojej organizacji. Ta domena jest używana do uwierzytelniania i innych usług systemowych.';
+$lang['openldap_directory_openldap_directory_driver'] = 'OpenLDAP katalog kierowcy';
+$lang['openldap_directory_openldap_directory_driver_description'] = 'Kierowca OpenLDAP dla usług katalogowych';
+$lang['openldap_directory_password_access'] = 'Hasło dostępu';
+$lang['openldap_directory_policies'] = 'polityk';
