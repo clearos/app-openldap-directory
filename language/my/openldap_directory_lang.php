38a20dcdc018fe7282986da3c896eb63b0189f7c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:31:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/openldap_directory_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['openldap_directory_accounts_access'] = 'Access ကိုအကောင့်';
+$lang['openldap_directory_accounts_access_invalid'] = 'ဝင်ရောက်ခွင့်မမှန်ကန်အကောင့်။';
+$lang['openldap_directory_accounts_dn'] = 'DN အကောင့်';
+$lang['openldap_directory_anonymous'] = 'အမည်မသိ';
+$lang['openldap_directory_app_description'] = 'အဆိုပါ Directory ကို Server ကိုအသုံးပြုသူများအုပ်စုများနှင့်စနစ်များအပေါ်သတင်းအချက်အလက်ပေးစွမ်းသည်။ သငျသညျပြည်တွင်းထုတ်နှင့်မိုဃ်းတိမ်၌တတိယပါတီ applications တွေကိုပေါင်းစပ်ရန်သင့် directory ကိုသာ access ကိုဖတ်ပေးနိုင်ပါသည်။';
+$lang['openldap_directory_app_name'] = 'directory ကို Server ကို';
+$lang['openldap_directory_initialize_help'] = 'သင့်ရဲ့ Directory ကို Server ကိုစတင်လုပ်ဆောင်တဲ့အခါမှာ, သင်သည်သင်၏အဖွဲ့အစည်းအတွက်အသုံးပြုခြေရင်းဒိုမိန်းကို select ပေးပါ။ ဒီဒိုမိန်း authentication ကိုနဲ့အခြား system န်ဆောင်မှုများအတွက်အသုံးပြုသည်။';
+$lang['openldap_directory_openldap_directory_driver'] = 'OpenLDAP Directory ကိုယာဉ်မောင်း';
+$lang['openldap_directory_openldap_directory_driver_description'] = 'directory ကိုန်ဆောင်မှုများအတွက် OpenLDAP ကားမောင်းသူ';
+$lang['openldap_directory_password_access'] = 'Password ကို Access ကို';
+$lang['openldap_directory_policies'] = 'မူဝါဒများ';
