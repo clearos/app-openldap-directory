44c02ed2d399ee3df991b3d2d0a8b54c48717fcb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:39:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/so/openldap_directory_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['openldap_directory_accounts_access'] = 'Koontada Access';
+$lang['openldap_directory_accounts_access_invalid'] = 'Xisaabaadka helaan waa baadil.';
+$lang['openldap_directory_accounts_dn'] = 'Koontada DN';
+$lang['openldap_directory_anonymous'] = 'Anonymous';
+$lang['openldap_directory_app_description'] = 'Server Directory wuxuu bixiyaa macluumaad ku saabsan dadka isticmaala, kooxaha iyo nidaamyada. Waxaad ku bixin kartaa akhriyo oo kaliya in aad buugga helaan dhexgalaan codsiyada dhinac saddexaad gudaha iyo daruurta labadaba.';
+$lang['openldap_directory_app_name'] = 'Server Directory';
+$lang['openldap_directory_initialize_help'] = 'Marka bilaabida Server Directory aad, fadlan dooro domain salka in aad isticmaasho si aad u urur. domain waxaa loo isticmaalaa sugida iyo adeegyada kale ee nidaamka.';
+$lang['openldap_directory_openldap_directory_driver'] = 'OpenLDAP Directory Driver';
+$lang['openldap_directory_openldap_directory_driver_description'] = 'darawalka OpenLDAP The adeegyada tusaha';
+$lang['openldap_directory_password_access'] = 'Password Access';
+$lang['openldap_directory_policies'] = 'Siyaasadaha';
