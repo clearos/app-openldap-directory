5180f4309bb513d7ab02cce44cc45b97d92e03e1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:34:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/openldap_directory_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['openldap_directory_accounts_access'] = 'fiókok hozzáférési';
+$lang['openldap_directory_accounts_access_invalid'] = 'Fiókok hozzáférési érvénytelen.';
+$lang['openldap_directory_accounts_dn'] = 'fiókok DN';
+$lang['openldap_directory_anonymous'] = 'Névtelen';
+$lang['openldap_directory_app_description'] = 'A Directory Server tájékoztatást nyújt a felhasználók, csoportok és rendszerek. Akkor adja meg a csak olvasható hozzáférést a könyvtár, hogy integrálja a harmadik féltől származó alkalmazások mind helyi, mind a felhő.';
+$lang['openldap_directory_app_name'] = 'Directory Server';
+$lang['openldap_directory_initialize_help'] = 'Inicializálásnál a Directory Server, kérjük, válassza ki a bázis tartományban, hogy használja a szervezet. Ez a domain hitelesítéshez használt és a rendszer egyéb szolgáltatások.';
+$lang['openldap_directory_openldap_directory_driver'] = 'OpenLDAP Directory illesztőprogram';
+$lang['openldap_directory_openldap_directory_driver_description'] = 'Az OpenLDAP driver címtárszolgáltatásokat';
+$lang['openldap_directory_password_access'] = 'Jelszó Access';
+$lang['openldap_directory_policies'] = 'házirendje';
