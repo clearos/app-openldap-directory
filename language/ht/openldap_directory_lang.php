b6d8bd886ef5ecba94889f376ac096feae94dda3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:33:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ht/openldap_directory_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['openldap_directory_accounts_access'] = 'kont Aksè';
+$lang['openldap_directory_accounts_access_invalid'] = 'Kont aksè se valab.';
+$lang['openldap_directory_accounts_dn'] = 'kont DN';
+$lang['openldap_directory_anonymous'] = 'Anonymous';
+$lang['openldap_directory_app_description'] = 'Sèvè a Anyè bay enfòmasyon sou itilizatè yo, gwoup yo ak sistèm yo. Ou ka bay li sèlman aksè nan anyè ou yo entegre aplikasyon pou twazyèm pati tou de lokalman ak nan nwaj la.';
+$lang['openldap_directory_app_name'] = 'Anyè sèvè';
+$lang['openldap_directory_initialize_help'] = 'Lè inisyalizin sèvè Anyè ou, tanpri chwazi domèn nan baz ke ou itilize pou òganizasyon w lan. se domèn sa a yo itilize pou Otantifikasyon ak sèvis sistèm ak lòt.';
+$lang['openldap_directory_openldap_directory_driver'] = 'Openldap Anyè chofè';
+$lang['openldap_directory_openldap_directory_driver_description'] = 'Chofè a openldap pou sèvis anyè';
+$lang['openldap_directory_password_access'] = 'Modpas Aksè';
+$lang['openldap_directory_policies'] = 'règleman';
