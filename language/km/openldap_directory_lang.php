e54ed9191fa088cd995771f847279af835e99b68
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:35:40 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/km/openldap_directory_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['openldap_directory_accounts_access'] = 'ការចូលដំណើរការគណនី';
+$lang['openldap_directory_accounts_access_invalid'] = 'គណនីចូលដំណើរការមិនត្រឹមត្រូវ។';
+$lang['openldap_directory_accounts_dn'] = 'គណនី DN';
+$lang['openldap_directory_anonymous'] = 'អនាមិក';
+$lang['openldap_directory_app_description'] = 'ម៉ាស៊ីនបម្រើថតបានផ្តល់ព័អ្នកប្រើ, ក្រុមនិងប្រព័ន្ធ។ អ្នកអាចផ្តល់នូវសិទ្ធិចូលដំណើរការអានតែមួយគត់ដើម្បីថតរបស់អ្នកដើម្បីរួមបញ្ចូលកម្មវិធីភាគីទីបីទាំងក្នុងស្រុកនិងនៅក្នុងពពក។';
+$lang['openldap_directory_app_name'] = 'ម៉ាស៊ីនបម្រើថត';
+$lang['openldap_directory_initialize_help'] = 'ពេលចាប់ផ្ដើមម៉ាស៊ីនបម្រើថតរបស់អ្នកសូមជ្រើសរើសដែនគោលដែលអ្នកបានប្រើសម្រាប់អង្គការរបស់អ្នក។ ដែននេះត្រូវបានប្រើសម្រាប់ការផ្ទៀងផ្ទាត់ភាពត្រឹមត្រូវនិងសេវាកម្មប្រព័ន្ធផ្សេងទៀត។';
+$lang['openldap_directory_openldap_directory_driver'] = 'កម្មវិធីបញ្ជាថតរបស់ OpenLDAP';
+$lang['openldap_directory_openldap_directory_driver_description'] = 'កម្មវិធីបញ្ជារបស់ OpenLDAP សម្រាប់សេវាថត';
+$lang['openldap_directory_password_access'] = 'ការចូលដំណើរការពាក្យសម្ងាត់';
+$lang['openldap_directory_policies'] = 'គោលនយោបាយ';
