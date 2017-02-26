234bf64e0e6498c5ff691146f667ca62b2d4b071
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:39:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/si/openldap_directory_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['openldap_directory_accounts_access'] = 'ගිණුම් ප්රවේශ';
+$lang['openldap_directory_accounts_access_invalid'] = 'ප්රවේශ සාවද්ය වේ ගිණුම්.';
+$lang['openldap_directory_accounts_dn'] = 'ගිණුම් ඩී.එන්';
+$lang['openldap_directory_anonymous'] = 'නිර්නාමික';
+$lang['openldap_directory_app_description'] = 'මෙම බහලුම් සේවාදායකය පරිශීලකයන්, කණ්ඩායම් හා පද්ධති පිළිබඳ තොරතුරු සපයයි. ඔබ දේශීය මෙන්ම වලාකුළු තුන්වන පාර්ශවීය වැඩසටහන් ඒකාබද්ධ කිරීමට ඔබගේ බහලුම වෙත ප්රවේශ පමණක් කියවිය ලබා දිය හැකිය.';
+$lang['openldap_directory_app_name'] = 'බහලුම් සේවාදායකය';
+$lang['openldap_directory_initialize_help'] = 'ඔබේ බහලුම් සේවාදායකය ආරම්භනය විට, කරුණාකර ඔබ ඔබගේ ආයතනය සඳහා භාවිතා කරන මූලික පදනමක් වසම තෝරන්න. මෙම වසම අනන්යතාවය සහ අනෙකුත් පද්ධති සේවා සඳහා භාවිතා කරනු ඇත.';
+$lang['openldap_directory_openldap_directory_driver'] = 'OpenLDAP නාමාවලිය ධාවක';
+$lang['openldap_directory_openldap_directory_driver_description'] = 'බහලුම සේවා සඳහා OpenLDAP රියදුරු';
+$lang['openldap_directory_password_access'] = 'මුරපදය ප්රවේශ';
+$lang['openldap_directory_policies'] = 'පිලිවෙත්';
