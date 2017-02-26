f4300283c730dff989470b3debb575c82edc0fc7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:41:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/openldap_directory_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['openldap_directory_accounts_access'] = 'cyfrifon Mynediad';
+$lang['openldap_directory_accounts_access_invalid'] = 'mynediad Cyfrifon yn annilys.';
+$lang['openldap_directory_accounts_dn'] = 'cyfrifon DN';
+$lang['openldap_directory_anonymous'] = 'Anonymous';
+$lang['openldap_directory_app_description'] = 'Mae&#39;r Gweinydd Cyfeiriadur darparu gwybodaeth am ddefnyddwyr, grwpiau a systemau. Gallwch ddarparu darllen mynediad yn unig at eich cyfeiriadur i integreiddio ceisiadau trydydd parti yn lleol ac yn y cwmwl.';
+$lang['openldap_directory_app_name'] = 'Cyfeiriadur Gweinydd';
+$lang['openldap_directory_initialize_help'] = 'Wrth ymgychwyn eich Gweinydd Cyfeiriadur, dewiswch y parth sylfaen eich bod yn defnyddio ar gyfer eich sefydliad. Mae&#39;r parth yn cael ei ddefnyddio ar gyfer dilysu a gwasanaethau system arall.';
+$lang['openldap_directory_openldap_directory_driver'] = 'Cyfeiriadur OpenLDAP Driver';
+$lang['openldap_directory_openldap_directory_driver_description'] = 'Mae&#39;r gyrrwr OpenLDAP gyfer gwasanaethau cyfeiriadur';
+$lang['openldap_directory_password_access'] = 'cyfrinair Mynediad';
+$lang['openldap_directory_policies'] = 'Polisïau';
