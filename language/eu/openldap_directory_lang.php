6e48df9c816df48f87700f650c43ac7a51bf3784
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:31:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eu/openldap_directory_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['openldap_directory_accounts_access'] = 'kontuak Access';
+$lang['openldap_directory_accounts_access_invalid'] = 'Kontuak sarbidea ez da zuzena.';
+$lang['openldap_directory_accounts_dn'] = 'kontuak DN';
+$lang['openldap_directory_anonymous'] = 'Anonymous';
+$lang['openldap_directory_app_description'] = 'Directory Server The erabiltzaileak, taldeak eta sistema buruzko informazioa eskaintzen du. Zure direktorioa sarbidea soilik hirugarren aplikazioak integratzeko hemengo eta hodeia irakurri eman dezakezu.';
+$lang['openldap_directory_app_name'] = 'Directory Server';
+$lang['openldap_directory_initialize_help'] = 'Zure Directory Server hasieratzeko, hautatu oinarri domeinu zure erakundeak erabiltzen duzula. Domeinu hori autentifikazio eta beste sistema zerbitzuak egiteko erabiltzen da.';
+$lang['openldap_directory_openldap_directory_driver'] = 'OpenLDAP Directory Driver';
+$lang['openldap_directory_openldap_directory_driver_description'] = 'OpenLDAP direktorioa zerbitzuen gidariari';
+$lang['openldap_directory_password_access'] = 'Pasahitza Access';
+$lang['openldap_directory_policies'] = 'Politikak';
