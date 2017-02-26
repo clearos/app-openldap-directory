7cb312043eac856ef8d6e6326dd07b84c30a5ef6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:40:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/openldap_directory_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['openldap_directory_accounts_access'] = 'суратҳисобҳои Access';
+$lang['openldap_directory_accounts_access_invalid'] = 'дастрасӣ ба ҳисоби нодуруст аст.';
+$lang['openldap_directory_accounts_dn'] = 'ҳисобҳои Д.Н';
+$lang['openldap_directory_anonymous'] = 'беимзо';
+$lang['openldap_directory_app_description'] = 'Дар Directory Server маълумот дар бораи истифодабарандагон, гурӯҳҳо ва системаҳои таъмин менамояд. Шумо метавонед таъмин хонда дастрасӣ танҳо ба директорияи шумо ҳамгиро замимаҳои тарафи сеюм ҳам маҳаллӣ ва ҳам дар абр.';
+$lang['openldap_directory_app_name'] = 'Directory Server';
+$lang['openldap_directory_initialize_help'] = 'Вақте ки initializing Directory Server кунед, лутфан домейн пойгоҳи ки ба шумо барои ташкили истифода кунед. Ин домейн аст, ки барои аутентификатсия ва дигар хизматрасониҳои низоми истифода бурда мешавад.';
+$lang['openldap_directory_openldap_directory_driver'] = 'Ронанда OpenLDAP Directory';
+$lang['openldap_directory_openldap_directory_driver_description'] = 'Ронанда OpenLDAP барои хизматрасонии феҳристи';
+$lang['openldap_directory_password_access'] = 'Рамз Access';
+$lang['openldap_directory_policies'] = 'сиёсати';
