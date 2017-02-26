686a523f58fe4326102c893a8af81f2e448fef85
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:37:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/nl/openldap_directory_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['openldap_directory_accounts_access'] = 'accounts Access';
+$lang['openldap_directory_accounts_access_invalid'] = 'Accounts toegang is ongeldig.';
+$lang['openldap_directory_accounts_dn'] = 'accounts DN';
+$lang['openldap_directory_anonymous'] = 'Anoniem';
+$lang['openldap_directory_app_description'] = 'De Directory Server bevat informatie over gebruikers, groepen en systemen. U kunt bieden alleen lezen toegang tot uw telefoonboek naar applicaties van derden, zowel lokaal als in de cloud te integreren.';
+$lang['openldap_directory_app_name'] = 'directory Server';
+$lang['openldap_directory_initialize_help'] = 'Wanneer het initialiseren van uw Directory Server, selecteert u de basis domein dat u gebruikt voor uw organisatie. Dit domein wordt gebruikt voor authenticatie en ander systeem services.';
+$lang['openldap_directory_openldap_directory_driver'] = 'OpenLDAP Directory Driver';
+$lang['openldap_directory_openldap_directory_driver_description'] = 'De OpenLDAP driver voor directory services';
+$lang['openldap_directory_password_access'] = 'Password Access';
+$lang['openldap_directory_policies'] = 'Policies';
