bfbba750ad3dfde7de475a25f83b5a488a26baad
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:31:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bn/openldap_directory_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['openldap_directory_accounts_access'] = 'অ্যাক্সেস অ্যাকাউন্ট';
+$lang['openldap_directory_accounts_access_invalid'] = 'অ্যাকাউন্ট অ্যাক্সেস অবৈধ.';
+$lang['openldap_directory_accounts_dn'] = 'ডিএন অ্যাকাউন্ট';
+$lang['openldap_directory_anonymous'] = 'নামবিহীন';
+$lang['openldap_directory_app_description'] = 'নির্দেশিকা সার্ভার ব্যবহারকারী, দল এবং সিস্টেমের উপর তথ্য প্রদান করে. আপনি তৃতীয় পক্ষের অ্যাপ্লিকেশন সংহত করতে উভয় স্থানীয়ভাবে এবং মেঘ আপনার নির্দেশিকাতে শুধুমাত্র প্রবেশাধিকার পড়া প্রদান করতে পারেন.';
+$lang['openldap_directory_app_name'] = 'নির্দেশিকা সার্ভার';
+$lang['openldap_directory_initialize_help'] = 'যখন আপনার নির্দেশিকা সার্ভার আরম্ভের, দয়া করে বেস ডোমেইন যে আপনি আপনার প্রতিষ্ঠানের জন্য ব্যবহার করুন নির্বাচন করুন. এই ডোমেন প্রমাণীকরণ এবং অন্যান্য সিস্টেম পরিষেবার জন্য ব্যবহার করা হয়.';
+$lang['openldap_directory_openldap_directory_driver'] = 'লেটি OpenLDAP নির্দেশিকা ড্রাইভার';
+$lang['openldap_directory_openldap_directory_driver_description'] = 'সেবা নির্দেশিকা জন্য লেটি OpenLDAP চালক';
+$lang['openldap_directory_password_access'] = 'পাসওয়ার্ড প্রবেশ';
+$lang['openldap_directory_policies'] = 'নীতিসমূহ';
