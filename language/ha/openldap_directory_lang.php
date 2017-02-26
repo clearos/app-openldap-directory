fd9e3fcb92fd68c52fde11ae1bc365bd56ec9cf9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:34:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ha/openldap_directory_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['openldap_directory_accounts_access'] = 'asusun Access';
+$lang['openldap_directory_accounts_access_invalid'] = 'Accounts access ba daidai ba ne.';
+$lang['openldap_directory_accounts_dn'] = 'asusun DN';
+$lang['openldap_directory_anonymous'] = 'Anonymous';
+$lang['openldap_directory_app_description'] = 'The Directory Server bayar da bayanai a kan masu amfani, kungiyoyi da tsarin. Za ka iya samar da karanta kawai access to your directory hade ɓangare na uku aikace-aikace a gida da kuma a cikin girgije.';
+$lang['openldap_directory_app_name'] = 'Directory Server';
+$lang['openldap_directory_initialize_help'] = 'A lokacin da soma your Directory Server, don Allah ka zaɓa tushe yankin da ka yi amfani da for your kungiyar. Wannan yankin da ake amfani da Tantance kalmar sirri da kuma sauran tsarin ayyuka.';
+$lang['openldap_directory_openldap_directory_driver'] = 'OpenLDAP Directory Driver';
+$lang['openldap_directory_openldap_directory_driver_description'] = 'The OpenLDAP direba ga directory ayyuka';
+$lang['openldap_directory_password_access'] = 'Password Access';
+$lang['openldap_directory_policies'] = 'Asiri';
