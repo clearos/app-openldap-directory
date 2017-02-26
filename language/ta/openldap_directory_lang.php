57f4e329196eee76598a589e836511f522eb4268
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:40:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ta/openldap_directory_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['openldap_directory_accounts_access'] = 'அணுகல் கணக்குகளைப்';
+$lang['openldap_directory_accounts_access_invalid'] = 'கணக்குகள் அணுகல் தவறானது.';
+$lang['openldap_directory_accounts_dn'] = 'டி.என் கணக்குகள்';
+$lang['openldap_directory_anonymous'] = 'பெயரில்லா';
+$lang['openldap_directory_app_description'] = 'அடைவு சேவையகம் பயனர்கள், குழுக்கள் மற்றும் அமைப்புகள் பற்றிய தகவல்களை வழங்குகிறது. நீங்கள் உள்நாட்டிலும் மேகம் மூன்றாம் தரப்பு பயன்பாடுகள் ஒருங்கிணைக்க உங்கள் அடைவு மட்டுமே அணுக படிக்க வழங்க முடியும்.';
+$lang['openldap_directory_app_name'] = 'அடைவு சேவையகம்';
+$lang['openldap_directory_initialize_help'] = 'உங்கள் அடைவு சேவையகம் ஆரம்பிக்கும் போது, நீங்கள் உங்கள் அமைப்பு பயன்படுத்தலாம் என்று அடிப்படை டொமைன் தேர்ந்தெடுக்கவும். இந்த டொமைன் அங்கீகாரம் மற்றும் இதர கணினி சேவைகள் பயன்படுத்தப்படுகிறது.';
+$lang['openldap_directory_openldap_directory_driver'] = 'OpenLDAP ஆனது ஏற்கனவே டிரைவர்';
+$lang['openldap_directory_openldap_directory_driver_description'] = 'அடைவு சேவைகள் OpenLDAP ஆனது இயக்கி';
+$lang['openldap_directory_password_access'] = 'கடவுச்சொல் அணுகல்';
+$lang['openldap_directory_policies'] = 'கொள்கைகள்';
