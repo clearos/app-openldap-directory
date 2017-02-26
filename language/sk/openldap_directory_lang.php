818a4ae252a489232ff76a39c9680d0114334b31
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:39:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sk/openldap_directory_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['openldap_directory_accounts_access'] = 'účty Prístup';
+$lang['openldap_directory_accounts_access_invalid'] = 'Účty prístupu je neplatná.';
+$lang['openldap_directory_accounts_dn'] = 'účty DN';
+$lang['openldap_directory_anonymous'] = 'Anonymný';
+$lang['openldap_directory_app_description'] = 'Directory Server poskytuje informácie o používateľov, skupín a systémov. Môžete poskytnúť len pre čítanie prístup k adresáru integrovať aplikácie tretích strán, a to ako na miestnej, tak v cloude.';
+$lang['openldap_directory_app_name'] = 'Directory Server';
+$lang['openldap_directory_initialize_help'] = 'Pri inicializácii vášho Directory Server, vyberte základné doménu, ktorú používate pre vašu organizáciu. Táto doména sa používa pre autentizáciu a iné systémové služby.';
+$lang['openldap_directory_openldap_directory_driver'] = 'OpenLDAP Directory Driver';
+$lang['openldap_directory_openldap_directory_driver_description'] = 'Vodič OpenLDAP pre adresárové služby';
+$lang['openldap_directory_password_access'] = 'heslo Prístup';
+$lang['openldap_directory_policies'] = 'Postupy';
