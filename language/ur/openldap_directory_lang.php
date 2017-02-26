f83f50767d688bc602530ef555d5452b27ceacbf
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:41:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ur/openldap_directory_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['openldap_directory_accounts_access'] = 'رسائی اکاؤنٹس';
+$lang['openldap_directory_accounts_access_invalid'] = 'اکاؤنٹس تک رسائی حاصل غلط ہے.';
+$lang['openldap_directory_accounts_dn'] = 'DN اکاؤنٹس';
+$lang['openldap_directory_anonymous'] = 'گمنام';
+$lang['openldap_directory_app_description'] = 'ڈائریکٹری سرور صارفین، گروہوں اور نظام کے بارے میں معلومات فراہم کرتا ہے. تم دونوں مقامی طور پر اور بادل میں تیسری پارٹی کی ایپلی کیشنز کو ضم کرنے کے لئے اپنے ڈائریکٹری میں صرف رسائی پڑھا فراہم کر سکتے ہیں.';
+$lang['openldap_directory_app_name'] = 'ڈائریکٹری سرور';
+$lang['openldap_directory_initialize_help'] = 'آپ ڈائریکٹری سرور ابتدا جب، بیس ڈومین آپ اپنی تنظیم کے لئے استعمال کرتے ہیں کہ منتخب کریں براہ مہربانی. یہ ڈومین تصدیق اور دوسرے نظام کی خدمات کے لئے استعمال کیا جاتا ہے.';
+$lang['openldap_directory_openldap_directory_driver'] = 'OpenLDAP کے ڈائریکٹری ڈرائیور';
+$lang['openldap_directory_openldap_directory_driver_description'] = 'کے لئے ڈائریکٹری خدمات OpenLDAP کے ڈرائیور';
+$lang['openldap_directory_password_access'] = 'پاس ورڈ تک رسائی';
+$lang['openldap_directory_policies'] = 'پالیسیاں';
