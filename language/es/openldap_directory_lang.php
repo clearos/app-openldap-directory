6af4f3fcf8476b4a1a29b45e2e9dfa8c5ebd6728
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:39:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/es/openldap_directory_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['openldap_directory_accounts_access'] = 'Las cuentas de acceso';
+$lang['openldap_directory_accounts_access_invalid'] = 'Acceso a cuentas no es válido.';
+$lang['openldap_directory_accounts_dn'] = 'Cuentas de DN';
+$lang['openldap_directory_anonymous'] = 'Anónimo';
+$lang['openldap_directory_app_description'] = 'El servidor de directorio proporciona información sobre usuarios, grupos y sistemas. Puede proporcionar acceso de sólo lectura a su directorio para integrar aplicaciones de terceros tanto a nivel local como en la nube.';
+$lang['openldap_directory_app_name'] = 'Directory Server';
+$lang['openldap_directory_initialize_help'] = 'Al inicializar el servidor de directorio, por favor seleccione el dominio de base que utiliza para su organización. Este dominio se utiliza para la autenticación y otros servicios del sistema.';
+$lang['openldap_directory_openldap_directory_driver'] = 'Directorio de OpenLDAP Conductor';
+$lang['openldap_directory_openldap_directory_driver_description'] = 'El conductor de OpenLDAP para servicios de directorio';
+$lang['openldap_directory_password_access'] = 'Contraseña de Acceso';
+$lang['openldap_directory_policies'] = 'políticas';
