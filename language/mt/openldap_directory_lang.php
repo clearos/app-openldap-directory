2e300598c58690ada5194074514b0139aec73790
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:37:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mt/openldap_directory_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['openldap_directory_accounts_access'] = 'kontijiet Aċċess';
+$lang['openldap_directory_accounts_access_invalid'] = 'Kontijiet aċċess huwa invalidu.';
+$lang['openldap_directory_accounts_dn'] = 'kontijiet DN';
+$lang['openldap_directory_anonymous'] = 'Anonymous';
+$lang['openldap_directory_app_description'] = 'Il Server Direttorju jipprovdi informazzjoni dwar l-utenti, gruppi u s-sistemi. Tista &#39;tipprovdi taqra aċċess biss għall tiegħek direttorju jintegraw applikazzjonijiet parti terza kemm lokalment kif ukoll fil-sħaba.';
+$lang['openldap_directory_app_name'] = 'Directory Server';
+$lang['openldap_directory_initialize_help'] = 'Meta initializing Direttorju Server tiegħek, jekk jogħġbok agħżel l-dominju bażi li tuża għall-organizzazzjoni tiegħek. Dan id-dominju huwa użat għal servizzi sistema oħra awtentikazzjoni u.';
+$lang['openldap_directory_openldap_directory_driver'] = 'OpenLDAP Direttorju Sewwieq';
+$lang['openldap_directory_openldap_directory_driver_description'] = 'Is-sewwieq OpenLDAP għas-servizzi tad-direttorju';
+$lang['openldap_directory_password_access'] = 'password Aċċess';
+$lang['openldap_directory_policies'] = 'politiki';
