9a3357a42f7525d665741667de42596763f92b53
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:39:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sr/openldap_directory_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['openldap_directory_accounts_access'] = 'рачуни Аццесс';
+$lang['openldap_directory_accounts_access_invalid'] = 'Рачуни приступ је неважећи.';
+$lang['openldap_directory_accounts_dn'] = 'рачуни ДН';
+$lang['openldap_directory_anonymous'] = 'анониман';
+$lang['openldap_directory_app_description'] = 'Директоријум сервер пружа информације о корисницима, групама и система. Можете обезбедити само за читање приступ свом каталогу за интеграцију апликација треће стране у земљи и у облаку.';
+$lang['openldap_directory_app_name'] = 'directory Сервер';
+$lang['openldap_directory_initialize_help'] = 'Када Иницијализацијом Дирецтори Сервер, изаберите базу домен који користите за вашу организацију. Овај домен се користи за проверу идентитета и других системских услуга.';
+$lang['openldap_directory_openldap_directory_driver'] = 'ОпенЛДАП Каталог Возач';
+$lang['openldap_directory_openldap_directory_driver_description'] = 'ОпенЛДАП возач за услуге директоријума';
+$lang['openldap_directory_password_access'] = 'пассворд приступ';
+$lang['openldap_directory_policies'] = 'politike';
