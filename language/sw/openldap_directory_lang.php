e89b1ba366eb18efd990630e4ddd0dc2f34ab720
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:40:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sw/openldap_directory_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['openldap_directory_accounts_access'] = 'Hesabu za Access';
+$lang['openldap_directory_accounts_access_invalid'] = 'Hesabu za upatikanaji ni batili.';
+$lang['openldap_directory_accounts_dn'] = 'Hesabu za DN';
+$lang['openldap_directory_anonymous'] = 'Anonymous';
+$lang['openldap_directory_app_description'] = 'Saraka Server hutoa habari juu ya watumiaji, vikundi na mifumo. Unaweza kutoa kusoma kupata tu kwa saraka yako ya kuunganisha tatu maombi ya upande wa wote wa ndani na katika hilo wingu.';
+$lang['openldap_directory_app_name'] = 'Saraka Server';
+$lang['openldap_directory_initialize_help'] = 'Wakati initializing Saraka Server yako, tafadhali kuchagua msingi uwanja kwamba matumizi kwa asasi yako. uwanja huu ni kutumika kwa ajili ya uthibitisho na huduma nyingine mfumo.';
+$lang['openldap_directory_openldap_directory_driver'] = 'OpenLDAP Saraka Dereva';
+$lang['openldap_directory_openldap_directory_driver_description'] = 'dereva OpenLDAP kwa ajili ya huduma directory';
+$lang['openldap_directory_password_access'] = 'nywila Access';
+$lang['openldap_directory_policies'] = 'sera';
