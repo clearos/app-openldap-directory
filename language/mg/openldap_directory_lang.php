ea06efcf2455fe1baeb75afc694fbabfb3160274
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:36:38 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/openldap_directory_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['openldap_directory_accounts_access'] = 'kaonty Access';
+$lang['openldap_directory_accounts_access_invalid'] = 'Kaonty fahafahana miditra dia tsy manan-kery.';
+$lang['openldap_directory_accounts_dn'] = 'kaonty DN';
+$lang['openldap_directory_anonymous'] = 'Anonymous';
+$lang['openldap_directory_app_description'] = 'Ny Directory Server manome vaovao momba ireo mpampiasa, vondrona sy ny rafitra. Azonao atao ny mamaky ihany no manome fidirana ny Directory ny mampiditra antoko fahatelo rindrambaiko na an-toerana sy eo amin&#39;ny rahona.';
+$lang['openldap_directory_app_name'] = 'Directory Server';
+$lang['openldap_directory_initialize_help'] = 'Rehefa nitranga teo am-ny Directory Server, dia mba mifidiana ny sehatra fototra izay ampiasaina noho ny fandaminana. Io sehatra dia ampiasaina ho fanamarinana sy ny rafitra hafa tolotra.';
+$lang['openldap_directory_openldap_directory_driver'] = 'OpenLDAP Directory Driver';
+$lang['openldap_directory_openldap_directory_driver_description'] = 'Ny OpenLDAP mpamily ny Directory asa';
+$lang['openldap_directory_password_access'] = 'Password Access';
+$lang['openldap_directory_policies'] = 'Policies';
