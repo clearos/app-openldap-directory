f975f804e39e33bcff639b85e8299aaf96a7cdb5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:33:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fi/openldap_directory_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['openldap_directory_accounts_access'] = 'Accounts Access';
+$lang['openldap_directory_accounts_access_invalid'] = 'Tilit pääsy on virheellinen.';
+$lang['openldap_directory_accounts_dn'] = 'Accounts DN';
+$lang['openldap_directory_anonymous'] = 'anonyymi';
+$lang['openldap_directory_app_description'] = 'Hakemisto Server tarjoaa tietoa käyttäjien, ryhmien ja järjestelmiä. Voit tarjota vain luku pääsyn hakemistoon integroida kolmannen osapuolen sovelluksia sekä paikallisesti pilvessä.';
+$lang['openldap_directory_app_name'] = 'Directory Server';
+$lang['openldap_directory_initialize_help'] = 'Kun alustetaan oman Directory Server, valitse pohja verkkotunnus, jota käytät organisaatiosi. Tämä verkkotunnus käytetään todentamiseen ja muiden järjestelmän palveluja.';
+$lang['openldap_directory_openldap_directory_driver'] = 'OpenLDAP Directory Kuljettajan';
+$lang['openldap_directory_openldap_directory_driver_description'] = 'OpenLDAP ajuri hakemistopalvelut';
+$lang['openldap_directory_password_access'] = 'salasana';
+$lang['openldap_directory_policies'] = 'säännöt';
