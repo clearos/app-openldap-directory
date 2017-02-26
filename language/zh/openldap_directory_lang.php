b1dcdde534fac50e7950445811fba4f56a11e47c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:32:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zh/openldap_directory_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['openldap_directory_accounts_access'] = '帐户的访问';
+$lang['openldap_directory_accounts_access_invalid'] = '帐户的访问是无效的。';
+$lang['openldap_directory_accounts_dn'] = '帐户DN';
+$lang['openldap_directory_anonymous'] = '匿名';
+$lang['openldap_directory_app_description'] = '目录服务器提供用户，组和系统的信息。 您可以提供只读到您的目录访问本地和云集成第三方应用程序。';
+$lang['openldap_directory_app_name'] = '目录服务器';
+$lang['openldap_directory_initialize_help'] = '当初始化目录服务器，请选择您使用为您的组织基础领域。 此域名用于身份验证等系统服务。';
+$lang['openldap_directory_openldap_directory_driver'] = 'OpenLDAP目录驱动程序';
+$lang['openldap_directory_openldap_directory_driver_description'] = 'OpenLDAP的驱动程序目录服务';
+$lang['openldap_directory_password_access'] = '密码访问';
+$lang['openldap_directory_policies'] = '政策';
