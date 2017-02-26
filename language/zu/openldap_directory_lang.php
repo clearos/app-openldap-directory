b5322c3b2d8f7b9f8790d1fa6809765e0147ed71
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:42:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zu/openldap_directory_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['openldap_directory_accounts_access'] = 'Thenga ama-akhawunti Access';
+$lang['openldap_directory_accounts_access_invalid'] = 'Ama-Akhawunti ukufinyelela ayivumelekile.';
+$lang['openldap_directory_accounts_dn'] = 'Thenga ama-akhawunti DN';
+$lang['openldap_directory_anonymous'] = 'Anonymous';
+$lang['openldap_directory_app_description'] = 'Server Directory inikeza ulwazi abasebenzisi, amaqembu kanye nezinhlelo. Ungakwazi ukunikeza ukufinyelela kokufunda kuphela isiqondisi sakho ukuze sihlanganise izinhlelo zokusebenza ezivela eceleni ngaphakathi ezweni nakumazwe efwini.';
+$lang['openldap_directory_app_name'] = 'Directory Server';
+$lang['openldap_directory_initialize_help'] = 'Lapho iqala Directory yakho Server, sicela ukhethe domain base oyisebenzisayo ukuze inhlangano yakho. Lesi sizinda usetshenziselwa ukufakazela ubuqiniso kanye nezinye izinsizakalo system.';
+$lang['openldap_directory_openldap_directory_driver'] = 'OpenLDAP Directory Driver';
+$lang['openldap_directory_openldap_directory_driver_description'] = 'I OpenLDAP umshayeli isiqondisi amasevisi';
+$lang['openldap_directory_password_access'] = 'Ukufinyelela Iphasiwedi';
+$lang['openldap_directory_policies'] = 'Izinqubomgomo';
