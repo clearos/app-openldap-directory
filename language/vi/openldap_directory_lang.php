d12bfb910b0222f9331c870ea6a7bf71012f53ae
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:41:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/openldap_directory_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['openldap_directory_accounts_access'] = 'tài khoản truy cập';
+$lang['openldap_directory_accounts_access_invalid'] = 'Tài khoản truy cập không hợp lệ.';
+$lang['openldap_directory_accounts_dn'] = 'tài khoản DN';
+$lang['openldap_directory_anonymous'] = 'Vô danh';
+$lang['openldap_directory_app_description'] = 'Directory Server cung cấp thông tin về người dùng, nhóm và hệ thống. Bạn có thể cung cấp chỉ đọc truy cập vào thư mục của bạn để tích hợp các ứng dụng của bên thứ ba cả nước và trong các đám mây.';
+$lang['openldap_directory_app_name'] = 'directory server';
+$lang['openldap_directory_initialize_help'] = 'Khi khởi tạo Directory Server của bạn, xin vui lòng chọn tên miền cơ sở mà bạn sử dụng cho tổ chức của bạn. Tên miền này được sử dụng để xác thực và các dịch vụ hệ thống khác.';
+$lang['openldap_directory_openldap_directory_driver'] = 'OpenLDAP mục driver';
+$lang['openldap_directory_openldap_directory_driver_description'] = 'Người lái xe OpenLDAP cho các dịch vụ thư mục';
+$lang['openldap_directory_password_access'] = 'Mật khẩu truy cập';
+$lang['openldap_directory_policies'] = 'Chính sách';
