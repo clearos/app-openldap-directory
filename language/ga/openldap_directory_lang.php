00347150f1138043ecf3217fe6d43132e79f1e45
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:34:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/openldap_directory_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['openldap_directory_accounts_access'] = 'Cuntais Rochtain';
+$lang['openldap_directory_accounts_access_invalid'] = 'Tá rochtain Cuntais neamhbhailí.';
+$lang['openldap_directory_accounts_dn'] = 'Cuntais DN';
+$lang['openldap_directory_anonymous'] = 'Anonymous';
+$lang['openldap_directory_app_description'] = 'Cuireann an Freastalaí Eolaire eolais ar úsáideoirí, grúpaí agus córais. Is féidir leat a chur ar fáil a léamh rochtain ach amháin do eolaire a chomhtháthú le hiarratais tríú páirtí go háitiúil agus sa scamall araon.';
+$lang['openldap_directory_app_name'] = 'eolaire Freastalaí';
+$lang['openldap_directory_initialize_help'] = 'Nuair initializing do Eolaire Server, roghnaigh an bhfearann ​​bonn a úsáideann tú le haghaidh do eagraíocht. Tá an fearann ​​a úsáidtear le haghaidh fíordheimhniú agus seirbhísí córas eile.';
+$lang['openldap_directory_openldap_directory_driver'] = 'OpenLDAP Eolaire Tiománaí';
+$lang['openldap_directory_openldap_directory_driver_description'] = 'An tiománaí OpenLDAP do sheirbhísí eolaire';
+$lang['openldap_directory_password_access'] = 'Pasfhocal Rochtain';
+$lang['openldap_directory_policies'] = 'Polasaithe';
