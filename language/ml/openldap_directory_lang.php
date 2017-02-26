5f39c966e4fcd2b54b68175c3440119bf68c4a02
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:37:03 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ml/openldap_directory_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['openldap_directory_accounts_access'] = 'പ്രവേശനം അക്കൗണ്ടുകൾ';
+$lang['openldap_directory_accounts_access_invalid'] = 'അക്കൗണ്ടുകൾ ആക്സസ് അസാധുവാണ്.';
+$lang['openldap_directory_accounts_dn'] = 'ഡിഎൻ അക്കൗണ്ട്സ്';
+$lang['openldap_directory_anonymous'] = 'പേരറിയാത്ത';
+$lang['openldap_directory_app_description'] = 'ഡയറക്ടറി സർവർ ഉപയോക്താക്കൾ, ഗ്രൂപ്പുകൾ, സിസ്റ്റങ്ങൾ വിവരങ്ങൾ നൽകുന്നു. നിങ്ങൾ രണ്ടുപേർക്കും പ്രാദേശികമായി ക്ലൗഡിൽ മൂന്നാം കക്ഷി അപ്ലിക്കേഷനുകൾ സമന്വയിപ്പിക്കുന്നതിന് നിങ്ങളുടെ ഡയറക്ടറിയുടെ വായന മാത്രം ആക്സസ്സ് നൽകാൻ കഴിയും.';
+$lang['openldap_directory_app_name'] = 'ഡയറക്ടറി സർവർ';
+$lang['openldap_directory_initialize_help'] = 'നിങ്ങളുടെ ഡയറക്ടറി സർവർ ആരംഭിക്കുന്പോൾ, നിങ്ങളുടെ ഓർഗനൈസേഷൻ ഉപയോഗിക്കുന്ന ബേസ് ഡൊമെയ്ൻ ദയവായി തിരഞ്ഞെടുക്കുക. ഈ ഡൊമെയ്ൻ ആധികാരികത മറ്റ് സിസ്റ്റം സേവനങ്ങൾ ഉപയോഗിക്കുന്നു.';
+$lang['openldap_directory_openldap_directory_driver'] = 'താളുകളിൽ ഡയറക്ടറി ഡ്രൈവർ';
+$lang['openldap_directory_openldap_directory_driver_description'] = 'ഡയറക്ടറി സേവനങ്ങൾക്കായി താളുകളിൽ ഡ്രൈവർ';
+$lang['openldap_directory_password_access'] = 'പാസ്വേഡ് ആക്സസ്';
+$lang['openldap_directory_policies'] = 'നയങ്ങള്';
