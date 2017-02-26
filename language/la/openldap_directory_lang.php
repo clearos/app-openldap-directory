b5c9a33efaddbdb1dc6d50defd696b4293a32df8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:36:05 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/la/openldap_directory_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['openldap_directory_accounts_access'] = 'Obvius accounts';
+$lang['openldap_directory_accounts_access_invalid'] = 'Ratio est invalidum aditus.';
+$lang['openldap_directory_accounts_dn'] = 'DN rationem';
+$lang['openldap_directory_anonymous'] = 'Sine nomine';
+$lang['openldap_directory_app_description'] = 'Directory Server praebet users de notitia, coetus et ratio. Tu solus legit obvius ut vestri possunt integrare presul et localiter ac tertius secui applications in nube.';
+$lang['openldap_directory_app_name'] = 'Directory Server';
+$lang['openldap_directory_initialize_help'] = 'Cum initializing Servo vestri presul, quod vos utor pro vestri Domain please select basi organizationem. Hoc enim est dominium et authenticas declarandas ratio muneris.';
+$lang['openldap_directory_openldap_directory_driver'] = 'Directory OpenLDAP Driver';
+$lang['openldap_directory_openldap_directory_driver_description'] = 'A presul pro auriga OpenLDAP Services';
+$lang['openldap_directory_password_access'] = 'Obvius Password';
+$lang['openldap_directory_policies'] = 'Politiae';
