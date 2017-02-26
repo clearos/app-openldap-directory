395f0e1213f8173be7163a6a21ca75bc816dda78
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:30:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/af/openldap_directory_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['openldap_directory_accounts_access'] = 'rekeninge Toegang';
+$lang['openldap_directory_accounts_access_invalid'] = 'Rekeninge toegang is ongeldig.';
+$lang['openldap_directory_accounts_dn'] = 'rekeninge DN';
+$lang['openldap_directory_anonymous'] = 'Anoniem';
+$lang['openldap_directory_app_description'] = 'Die Gids Server verskaf inligting oor die gebruikers, groepe en stelsels. Jy kan verskaf lees net toegang tot jou gids tot die derde party programme plaaslik sowel as in die wolk te integreer.';
+$lang['openldap_directory_app_name'] = 'Gids Server';
+$lang['openldap_directory_initialize_help'] = 'Wanneer inisialisering jou Gids Server, kies asseblief die basis domein wat u gebruik vir jou organisasie. Dit domein is wat gebruik word vir verifikasie en ander stelsel dienste.';
+$lang['openldap_directory_openldap_directory_driver'] = 'OpenLDAP Gids Driver';
+$lang['openldap_directory_openldap_directory_driver_description'] = 'Die OpenLDAP bestuurder vir directory services';
+$lang['openldap_directory_password_access'] = 'wagwoord Access';
+$lang['openldap_directory_policies'] = 'beleid';
