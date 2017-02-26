f2ee86cbf028abad917b5575c9d037b866f95c64
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:35:23 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ka/openldap_directory_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['openldap_directory_accounts_access'] = 'ანგარიშები წვდომა';
+$lang['openldap_directory_accounts_access_invalid'] = 'ანგარიშები ხელმისაწვდომი არასწორია.';
+$lang['openldap_directory_accounts_dn'] = 'ანგარიშები DN';
+$lang['openldap_directory_anonymous'] = 'Anonymous';
+$lang['openldap_directory_app_description'] = 'ცნობარი Server უზრუნველყოფს ინფორმაციის მომხმარებლებს, ჯგუფები და სისტემები. თქვენ შეგიძლიათ უზრუნველყოფს წაკითხული მხოლოდ ხელმისაწვდომობის თქვენი დირექტორია ინტეგრაცია მესამე მხარის განაცხადების როგორც ადგილობრივ და ღრუბელი.';
+$lang['openldap_directory_app_name'] = 'დირექტორია Server';
+$lang['openldap_directory_initialize_help'] = 'როდესაც ინიციალიზაციისას თქვენი დირექტორია Server, გთხოვთ ბაზის domain, რომ თქვენ იყენებთ თქვენი ორგანიზაცია. ეს domain გამოიყენება ავტორიზაციის და სხვა სისტემის მომსახურება.';
+$lang['openldap_directory_openldap_directory_driver'] = 'OpenLDAP დირექტორია Driver';
+$lang['openldap_directory_openldap_directory_driver_description'] = 'OpenLDAP მძღოლი დირექტორია მომსახურება';
+$lang['openldap_directory_password_access'] = 'პაროლი წვდომის';
+$lang['openldap_directory_policies'] = 'პოლიტიკა';
