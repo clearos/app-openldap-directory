bb3dcfabf3de2a5a947b8619a370d8f80cc4e734
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:42:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yi/openldap_directory_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['openldap_directory_accounts_access'] = 'אַקקאָונץ אַקסעס';
+$lang['openldap_directory_accounts_access_invalid'] = 'אַקקאָונץ צוטריט איז פאַרקריפּלט.';
+$lang['openldap_directory_accounts_dn'] = 'אַקקאָונץ דן';
+$lang['openldap_directory_anonymous'] = 'אַנאָנימע באַנוצערס';
+$lang['openldap_directory_app_description'] = 'די דירעקטארי סערווירער גיט אינפֿאָרמאַציע אויף ניצערס, גרופּעס און סיסטעמען. איר קענען צושטעלן לייענען בלויז צוטריט צו דיין Directory צו ויסשטימען דריט פּאַרטיי פּראָגראַמען ביידע לאָוקאַלי און אין דעם וואָלקן.';
+$lang['openldap_directory_app_name'] = 'דירעקטארי סערווירער';
+$lang['openldap_directory_initialize_help'] = 'ווען יניטיאַליזינג דיין דירעקטארי סערווירער, ביטע סעלעקטירן דעם באַזע פעלד אַז איר נוצן פֿאַר דיין אָרגאַניזאַציע. דאס פעלד איז געניצט פֿאַר אָטענטאַקיישאַן און אנדערע סיסטעם באַדינונגען.';
+$lang['openldap_directory_openldap_directory_driver'] = 'אָפּענלדאַפּ דירעקטארי דרייווער';
+$lang['openldap_directory_openldap_directory_driver_description'] = 'די אָפּענלדאַפּ שאָפער פֿאַר Directory סערוויסעס';
+$lang['openldap_directory_password_access'] = 'שפּריכוואָרט אַקסעס';
+$lang['openldap_directory_policies'] = 'פּאָליסיעס';
