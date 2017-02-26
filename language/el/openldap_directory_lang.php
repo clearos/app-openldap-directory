ad1c85985fa0bfc2c7dc048143c0196979c8448f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:33:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/el/openldap_directory_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['openldap_directory_accounts_access'] = 'λογαριασμοί Access';
+$lang['openldap_directory_accounts_access_invalid'] = 'πρόσβασης λογαριασμών είναι άκυρη.';
+$lang['openldap_directory_accounts_dn'] = 'λογαριασμοί DN';
+$lang['openldap_directory_anonymous'] = 'Ανώνυμος';
+$lang['openldap_directory_app_description'] = 'Ο διακομιστής καταλόγου παρέχει πληροφορίες για τους χρήστες, τις ομάδες και τα συστήματα. Μπορείτε να παρέχουν πρόσβαση μόνο για ανάγνωση στον κατάλογο σας για να ενσωματώσουν εφαρμογές τρίτων, τόσο τοπικά όσο και στο σύννεφο.';
+$lang['openldap_directory_app_name'] = 'Directory Server';
+$lang['openldap_directory_initialize_help'] = 'Όταν προετοιμασία Directory Server σας, παρακαλούμε επιλέξτε τον τομέα βάσης που χρησιμοποιείτε για τον οργανισμό σας. Αυτός ο τομέας χρησιμοποιείται για τον έλεγχο ταυτότητας και άλλες υπηρεσίες του συστήματος.';
+$lang['openldap_directory_openldap_directory_driver'] = 'OpenLDAP Directory Driver';
+$lang['openldap_directory_openldap_directory_driver_description'] = 'Ο οδηγός OpenLDAP για υπηρεσίες καταλόγου';
+$lang['openldap_directory_password_access'] = 'τον κωδικό πρόσβασης';
+$lang['openldap_directory_policies'] = 'πολιτικές';
