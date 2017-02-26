04d4c9462779a35b7d06d5815893a34c01225716
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:35:48 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kn/openldap_directory_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['openldap_directory_accounts_access'] = 'ಖಾತೆಗಳನ್ನು ಪ್ರವೇಶಿಸಲು';
+$lang['openldap_directory_accounts_access_invalid'] = 'ಅಕೌಂಟ್ಸ್ ಪ್ರವೇಶಿಸಿ ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['openldap_directory_accounts_dn'] = 'ಡಿ ಖಾತೆಗಳು';
+$lang['openldap_directory_anonymous'] = 'ಅನಾಮಧೇಯ';
+$lang['openldap_directory_app_description'] = 'ಡಿರಕ್ಟರಿ ಬಳಕೆದಾರರು, ಸಮೂಹಗಳು ಮತ್ತು ಗಣಕಗಳಲ್ಲಿ ಮಾಹಿತಿಯನ್ನು ಒದಗಿಸುತ್ತದೆ. ನೀವು ಎರಡೂ ಸ್ಥಳೀಯವಾಗಿ ಮತ್ತು ಮೋಡದಲ್ಲಿ ಮೂರನೇ ಪಕ್ಷದ ಅಪ್ಲಿಕೇಶನ್ಗಳನ್ನು ಸಂಯೋಜಿಸಲು ನಿಮ್ಮ ಕೋಶವನ್ನು ಓದಲು ಮಾತ್ರ ಪ್ರವೇಶ ಒದಗಿಸುತ್ತದೆ.';
+$lang['openldap_directory_app_name'] = 'ಡಿರಕ್ಟರಿ';
+$lang['openldap_directory_initialize_help'] = 'ನಿಮ್ಮ ಡಿರಕ್ಟರಿ ಆರಂಭಿಸುವಲ್ಲಿ ಮಾಡಿದಾಗ, ನಿಮ್ಮ ಸಂಸ್ಥೆಗೆ ಬಳಸುವ ಬೇಸ್ ಡೊಮೇನ್ ಆಯ್ಕೆ ಮಾಡಿ. ಈ ಡೊಮೇನ್ ದೃಢೀಕರಣ ಮತ್ತು ಇತರ ವ್ಯವಸ್ಥೆಯ ಸೇವೆಗಳನ್ನು ಬಳಸಲಾಗುತ್ತದೆ.';
+$lang['openldap_directory_openldap_directory_driver'] = 'OpenLDAP ಅನ್ನು ಡೈರೆಕ್ಟರಿ ಚಾಲಕ';
+$lang['openldap_directory_openldap_directory_driver_description'] = 'ಕೈಪಿಡಿ ಸೇವೆಗಳಿಗೆ OpenLDAP ಅನ್ನು ಚಾಲಕ';
+$lang['openldap_directory_password_access'] = 'ಪಾಸ್ವರ್ಡ್ ಪ್ರವೇಶ';
+$lang['openldap_directory_policies'] = 'ನೀತಿಗಳು';
