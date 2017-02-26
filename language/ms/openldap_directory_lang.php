67043338370ff6957370d35e9d58f339f1d0372a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:37:28 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ms/openldap_directory_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['openldap_directory_accounts_access'] = 'akaun Access';
+$lang['openldap_directory_accounts_access_invalid'] = 'akses akaun tidak sah.';
+$lang['openldap_directory_accounts_dn'] = 'akaun DN';
+$lang['openldap_directory_anonymous'] = 'Anonymous';
+$lang['openldap_directory_app_description'] = 'Direktori Server menyediakan maklumat mengenai pengguna, kumpulan dan sistem. Anda boleh memberikan bacaan sahaja akses kepada direktori anda untuk mengintegrasikan aplikasi pihak ketiga di dalam dan di dalam awan.';
+$lang['openldap_directory_app_name'] = 'Direktori Server';
+$lang['openldap_directory_initialize_help'] = 'Apabila Memulakan Server Direktori anda, sila pilih domain asas yang anda gunakan untuk organisasi anda. domain ini digunakan untuk pengesahan dan perkhidmatan sistem lain.';
+$lang['openldap_directory_openldap_directory_driver'] = 'Direktori OpenLDAP pemandu';
+$lang['openldap_directory_openldap_directory_driver_description'] = 'Pemandu OpenLDAP untuk perkhidmatan direktori';
+$lang['openldap_directory_password_access'] = 'Kata laluan Access';
+$lang['openldap_directory_policies'] = 'Polisi';
