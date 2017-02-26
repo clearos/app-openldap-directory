ad2aac1b7e2830e7a54e8fcf8f85b356a2a04cf1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:31:48 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bg/openldap_directory_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['openldap_directory_accounts_access'] = 'сметки Access';
+$lang['openldap_directory_accounts_access_invalid'] = 'Сметки достъп е невалиден.';
+$lang['openldap_directory_accounts_dn'] = 'сметки DN';
+$lang['openldap_directory_anonymous'] = 'анонимен';
+$lang['openldap_directory_app_description'] = 'Сървър справочник предоставя информация за потребители, групи и системи. Можете да се осигури достъп само за четене до вашата директория за интегриране на приложения на трети лица в страната и в облака.';
+$lang['openldap_directory_app_name'] = 'Directory сървър';
+$lang['openldap_directory_initialize_help'] = 'Когато инициализиране си Directory сървър, моля изберете основния домейн, който използвате за вашата организация. Този домейн се използва за удостоверяване и други системни услуги.';
+$lang['openldap_directory_openldap_directory_driver'] = 'OpenLDAP Directory Driver';
+$lang['openldap_directory_openldap_directory_driver_description'] = 'Шофьорът OpenLDAP за справочни услуги';
+$lang['openldap_directory_password_access'] = 'парола за достъп';
+$lang['openldap_directory_policies'] = 'политики';
