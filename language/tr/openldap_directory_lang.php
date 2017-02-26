a0d09a832e74ab265e285c8070cf743a5c87e573
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:41:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tr/openldap_directory_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['openldap_directory_accounts_access'] = 'Erişim hesapları';
+$lang['openldap_directory_accounts_access_invalid'] = 'Hesaplar erişim geçersiz.';
+$lang['openldap_directory_accounts_dn'] = 'DN hesapları';
+$lang['openldap_directory_anonymous'] = 'Anonim';
+$lang['openldap_directory_app_description'] = 'Dizin Server kullanıcıları, grupları ve sistemleri hakkında bilgi sağlar. Sen hem lokal hem de bulutta üçüncü parti uygulamaları entegre etmek için dizinin sadece okuma erişimi sağlayabilir.';
+$lang['openldap_directory_app_name'] = 'Dizin Sunucusu';
+$lang['openldap_directory_initialize_help'] = 'senin dizin sunucusu başlatılırken, kuruluşunuz için kullanmak taban alanını seçiniz. Bu etki alanı kimlik ve diğer sistem hizmetleri için kullanılmaktadır.';
+$lang['openldap_directory_openldap_directory_driver'] = 'OpenLDAP Directory Sürücü';
+$lang['openldap_directory_openldap_directory_driver_description'] = 'dizin hizmetleri için OpenLDAP sürücüsü';
+$lang['openldap_directory_password_access'] = 'Şifre Erişim';
+$lang['openldap_directory_policies'] = 'İlkeler';
