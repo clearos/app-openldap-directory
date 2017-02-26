8a8c4309f4ecedd2270649655a73bd7946272aaa
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:39:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sl/openldap_directory_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['openldap_directory_accounts_access'] = 'računi dostop';
+$lang['openldap_directory_accounts_access_invalid'] = 'Računi dostop je neveljavna.';
+$lang['openldap_directory_accounts_dn'] = 'računov DN';
+$lang['openldap_directory_anonymous'] = 'Anonymous';
+$lang['openldap_directory_app_description'] = 'Directory Server nudi informacije o uporabnikov, skupin in sistemov. Lahko zagotoviti bralni dostop do imenika za integracijo aplikacij tretjih oseb, tako na lokalni ravni in v oblaku.';
+$lang['openldap_directory_app_name'] = 'Directory Server';
+$lang['openldap_directory_initialize_help'] = 'Ko inicializacijo svojo Directory Server, izberite osnovno domeno, ki jo uporabljate za vašo organizacijo. To področje se uporablja za preverjanje pristnosti in drugih sistemskih storitev.';
+$lang['openldap_directory_openldap_directory_driver'] = 'OpenLDAP Imenik Driver';
+$lang['openldap_directory_openldap_directory_driver_description'] = 'OpenLDAP gonilnik za imeniške storitve';
+$lang['openldap_directory_password_access'] = 'geslo dostop';
+$lang['openldap_directory_policies'] = 'Pravila';
