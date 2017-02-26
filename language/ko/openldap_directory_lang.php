836d78cda3c409707fd5ba7b5741fbe5292c66c8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:35:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ko/openldap_directory_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['openldap_directory_accounts_access'] = '액세스 계정';
+$lang['openldap_directory_accounts_access_invalid'] = '계정 액세스가 잘못되었습니다.';
+$lang['openldap_directory_accounts_dn'] = 'DN은 계정';
+$lang['openldap_directory_anonymous'] = '익명';
+$lang['openldap_directory_app_description'] = '디렉토리 서버는 사용자, 그룹 및 시스템에 대한 정보를 제공합니다. 당신은 로컬 및 클라우드에서 타사 응용 프로그램을 통합 할 수있는 디렉토리에 대한 읽기 전용 액세스를 제공 할 수 있습니다.';
+$lang['openldap_directory_app_name'] = '디렉토리 서버';
+$lang['openldap_directory_initialize_help'] = '디렉토리 서버를 초기화 할 때, 당신은 당신의 조직에 사용하는 기본 도메인을 선택하십시오. 이 도메인은 인증 및 기타 시스템 서비스에 사용됩니다.';
+$lang['openldap_directory_openldap_directory_driver'] = 'OpenLDAP를 디렉토리 드라이버';
+$lang['openldap_directory_openldap_directory_driver_description'] = '디렉토리 서비스에 대한 OpenLDAP를 드라이버';
+$lang['openldap_directory_password_access'] = '암호 액세스';
+$lang['openldap_directory_policies'] = '정책';
