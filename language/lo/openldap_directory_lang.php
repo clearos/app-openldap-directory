08e9002c044d7904fff99917435b39325c5a2eee
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:36:13 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lo/openldap_directory_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['openldap_directory_accounts_access'] = 'ບັນຊີການເຂົ້າເຖິງ';
+$lang['openldap_directory_accounts_access_invalid'] = 'ການເຂົ້າເຖິງບັນຊີບໍ່ຖືກຕ້ອງ.';
+$lang['openldap_directory_accounts_dn'] = 'ບັນຊີ DN';
+$lang['openldap_directory_anonymous'] = 'Anonymous';
+$lang['openldap_directory_app_description'] = 'The Server Directory ໃຫ້ຂໍ້ມູນກ່ຽວກັບຜູ້ໃຊ້, ກຸ່ມແລະລະບົບ. ທ່ານສາມາດສະຫນອງອ່ານເຂົ້າເຖິງພຽງແຕ່ລະບົບຂອງທ່ານເຊື່ອມໂຍງກັບຄໍາຮ້ອງສະຫມັກພາກສ່ວນທີສາມທັງສອງຢູ່ໃນທ້ອງຖິ່ນແລະໃນການຟັງໄດ້.';
+$lang['openldap_directory_app_name'] = 'Server Directory';
+$lang['openldap_directory_initialize_help'] = 'ໃນເວລາທີ່ເລີ່ມຕົ້ນ Server ລະບົບຂອງທ່ານ, ກະລຸນາເລືອກໂດເມນຖານທີ່ທີ່ທ່ານໃຊ້ສໍາລັບອົງການຈັດຕັ້ງຂອງທ່ານ. ໂດເມນນີ້ໄດ້ຖືກນໍາໃຊ້ສໍາລັບການກວດສອບແລະບໍລິການລະບົບອື່ນໆ.';
+$lang['openldap_directory_openldap_directory_driver'] = 'Driver OpenLDAP Directory';
+$lang['openldap_directory_openldap_directory_driver_description'] = 'ຄົນຂັບ OpenLDAP ສໍາລັບການບໍລິການລະບົບ';
+$lang['openldap_directory_password_access'] = 'ການເຂົ້າລະຫັດຜ່ານ';
+$lang['openldap_directory_policies'] = 'ນະໂຍບາຍ';
