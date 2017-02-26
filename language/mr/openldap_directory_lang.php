6d16af089d7f9e6cfacfc0e6ea6ba0f767a44b51
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:37:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mr/openldap_directory_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['openldap_directory_accounts_access'] = 'प्रवेश खाती';
+$lang['openldap_directory_accounts_access_invalid'] = 'खात्यांत प्रवेश अवैध आहे.';
+$lang['openldap_directory_accounts_dn'] = 'DN खाती';
+$lang['openldap_directory_anonymous'] = 'अनामित';
+$lang['openldap_directory_app_description'] = 'निर्देशिका सेवक वर वापरकर्ते, गट आणि प्रणाली माहिती पुरवतो. आपण तृतीय पक्ष अनुप्रयोग स्थानिक आणि ढग दोन्ही समाकलित करण्याची आपल्या निर्देशिका फक्त प्रवेश वाचा प्रदान करू शकता.';
+$lang['openldap_directory_app_name'] = 'निर्देशिका सेवक';
+$lang['openldap_directory_initialize_help'] = 'आपल्या निर्देशिका सेवक सुरू असताना, कृपया बेस डोमेन आपण आपल्या संस्थेसाठी वापर सिलेक्ट करा. हे डोमेन प्रमाणीकरण आणि इतर प्रणाली सेवा वापरले जाते.';
+$lang['openldap_directory_openldap_directory_driver'] = 'ओपन LDAP डिरेक्ट्री ड्राइवर';
+$lang['openldap_directory_openldap_directory_driver_description'] = 'निर्देशिका सेवा ओपन LDAP ड्राइव्हर';
+$lang['openldap_directory_password_access'] = 'पासवर्ड प्रवेश';
+$lang['openldap_directory_policies'] = 'धोरणे';
